da54847f9ac4f6ef47153b9013652792a925c397
glopezhe97 <gerlohe97@gmail.com>
2025-12-11 20:48:17 +0100

    fix https

--- ritm-hub-front/src/environments/environment.ts
+++ ritm-hub-front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ritm-hub-backend-production.up.railway.app'
+    apiUrl: 'https://ritm-hub-backend-production.up.railway.app'
 };
