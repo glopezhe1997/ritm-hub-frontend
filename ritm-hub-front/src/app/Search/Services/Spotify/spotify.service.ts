94869bf971ce24a578e1d677d9db9dd9f3013574
glopezhe97 <gerlohe97@gmail.com>
2025-12-12 20:04:38 +0100

    RTM-Fix spotify url

--- ritm-hub-front/src/app/Search/Services/Spotify/spotify.service.ts
+++ ritm-hub-front/src/app/Search/Services/Spotify/spotify.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-  private apiUrl = 'http://localhost:3000/spotify';
+
+  private url = `${environment.apiUrl}/spotify`;
 
   constructor(
     private http: HttpClient,
@@ -14,12 +16,12 @@ export class SpotifyService {
 
   search(q: string): Observable<any> {
     const type = ['artist', 'album', 'track', 'playlist'].join(',');
-    const endpoint = `${this.apiUrl}/search?q=${encodeURIComponent(q)}`;
+    const endpoint = `${this.url}/search?q=${encodeURIComponent(q)}`;
     return this.http.get<any>(endpoint);
   }
 
   postSearch(id: string, type: string): Observable<any> {
-    const endpoint = `${this.apiUrl}/select`;
+    const endpoint = `${this.url}/select`;
     return this.http.post<any>(endpoint, { external_id: id, type });
   }
 }
